d9edee46bc09c8559e859ec14c971748e6290b2a
liv-szt <oliviasztanga@gmail.com>
2020-07-21 15:20:48 -0400

    Updates

--- src/assets/content/links.js
+++
@@ -1,3 +0,0 @@
-export const github = "https://www.github.com/oliviasztanga"
-export const linkedin = "https://www.linkedin.com/in/oliviasztanga"
-export const email = "mailto:oliviasztanga@gmail.com"
